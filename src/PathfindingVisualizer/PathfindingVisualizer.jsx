290eb391d595b5d09374b76f3cc3653a741f2c2c
Akshit Thakur <akshit2649@gmail.com>
2022-09-23 19:00:34 +0530

    refactoring

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,34 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './PathfindingVisualizer.css';
 import Node from './Node/Node';
 
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
+
+//Inital setting of the grid
+
+const createNode = (row, col) => {
+  return {
+    row,
+    col,
+    isStart: START_NODE_ROW === row && START_NODE_COL === col,
+    isFinish: FINISH_NODE_ROW === row && FINISH_NODE_COL === col,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+
+const grid = [];
+for (let row = 1; row <= 15; row++) {
+  const currentRow = [];
+  for (let col = 1; col <= 50; col++) {
+    currentRow.push(createNode(row, col));
+  }
+  grid.push(currentRow);
+}
+
 const PathfindingVisualizer = () => {
-  const [node, setNode] = useState([]);
-
-  useEffect(() => {
-    const rowNodes = [];
-    for (let row = 1; row <= 15; row++) {
-      const currentCol = [];
-      for (let col = 1; col <= 50; col++) {
-        const currentNode = {
-          row,
-          col,
-          isStart: row === 10 && col === 5,
-          isFinish: row === 10 && col === 45,
-        };
-        currentCol.push(currentNode);
-      }
-      rowNodes.push(currentCol);
-    }
-    setNode(rowNodes);
-  }, []);
-
   return (
     <div className="grid">
-      {node.map((row, rowIdx) => (
+      {grid.map((row, rowIdx) => (
         <div key={rowIdx}>
-          {row.map((ele, nodeIdx) => {
-            const { isStart, isFinish } = ele;
+          {row.map((node, nodeIdx) => {
+            const { isStart, isFinish } = node;
             return <Node key={nodeIdx} isStart={isStart} isFinish={isFinish} />;
           })}
         </div>
